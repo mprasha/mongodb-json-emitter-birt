797d15d74cd7e9014fe14f6abd0becd5c4ca85d5
arpandahal <arpandahal@gmail.com>
2013-04-23 21:30:04 +0000

    First checkin. Mongodb and JSON emitter. Yippee....

---
+++ Json Emitter/src/org/eclipse/birt/report/engine/emitter/json/common/JsonEmitterConstants.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2013 Megha Nidhi Dahal. All rights reserved. 
+ * This program and the accompanying materials are made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: Megha Nidhi Dahal
+ */
+package org.eclipse.birt.report.engine.emitter.json.common;
+
+public class JsonEmitterConstants {
+
+	public static final String RENDEROPTION_OUTPUT_DIR = "OUTPUT_DIR";
+	public static final String JSON_FILE_EXTENSION = ".json";
+}
