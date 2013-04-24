97bb45fa5b78beafc4ae553c5568e8c284ffd134
arpandahal@gmail.com <arpandahal@gmail.com@45995c24-49d0-3d2f-1d49-5e66cce39a44>
2013-04-24 13:20:47 +0000

    Renaming the code structure..

---
+++ trunk/org.eclipse.birt.report.engine.emitter.mongodb/src/org/eclipse/birt/report/engine/emitter/mongodb/common/MongoDbEmitterConstants.java
@@ -0,0 +1,8 @@
+package org.eclipse.birt.report.engine.emitter.mongodb.common;
+
+public class MongoDbEmitterConstants {
+
+	public static final String OPTION_MONGODB_HOST_ADDRESS = "MONGODB_HOST";
+	public static final String OPTION_MONGODB_PORT = "MONGODB_PORT";
+	public static final String OPTION_MONGODB_DB_NAME = "MONGODB_DB";
+}
